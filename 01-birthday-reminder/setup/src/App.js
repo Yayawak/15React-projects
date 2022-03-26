a06648c0aa737fe0e375a089b0fce270f14f6588
Yayawak <elio.interesante@gmail.com>
2022-03-26 16:23:35 +0700

    Setup birthday-reminder project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+    return
+        <main>
+            
+        </main>
 }
 
 export default App;
