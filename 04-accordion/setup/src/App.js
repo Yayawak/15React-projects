0b0300b20e89b0c47fe1e503196dd88b9dc9a0ab
Yayawak <elio.interesante@gmail.com>
2022-03-27 00:00:44 +0700

    Finished Review.js with toggling and iterating array

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
+import Question from './Question';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question,setQuestion] = useState(null);
+
+
+  const questionElements = data.map((qust) => {
+    return <Question {...qust} key={qust.id}/>
+    
+  })
+  return (
+    <main className='container'>
+      <section className=''>
+        <h3>Questions And Answers About Login</h3>
+      </section>
+      <section className=''>
+        {questionElements}
+      </section>
+    </main>
+  )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({id,title,info}) => {
+  const [isShowInfo,setIsShowInfo] = useState(false);
+  return (
+      <div className='question'>
+        <p>
+          <h4 className='header'>
+            {title}
+            <button className='btn' onClick={() => {
+              setIsShowInfo(!isShowInfo)
+            }}>
+              { isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus/>}
+            </button>
+          </h4>
+        </p>
+        <p>{isShowInfo ? info : ""}</p>
+      </div>
+  )
 };
 
 export default Question;
