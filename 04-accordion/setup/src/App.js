a32620aa7c216ebeb25a224158e90e3bed71d5e6
Yayawak <elio.interesante@gmail.com>
2022-03-27 00:03:23 +0700

    change bg pink->blue

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import data from './data';
 import Question from './Question';
-import SingleQuestion from './Question';
 function App() {
-  const [question,setQuestion] = useState(null);
-
-
-  const questionElements = data.map((qust) => {
+  // const [question,setQuestion] = useState(null);
+  // const questionElements = data.map((qust) => {
+  //   return <Question {...qust} key={qust.id}/>
+    
+  // })
+  const [questions, setQuestions] = useState(data);
+  const questionElements = questions.map((qust) => {
     return <Question {...qust} key={qust.id}/>
     
   })
   return (
-    <main className='container'>
-      <section className=''>
+    <main >
+      <div className='container'>
         <h3>Questions And Answers About Login</h3>
-      </section>
-      <section className=''>
-        {questionElements}
-      </section>
+        <section>
+          {
+            questionElements
+          }
+        </section>
+      </div>
+      
     </main>
   )
 }
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = ({id,title,info}) => {
+const Question = ({title,info}) => {
   const [isShowInfo,setIsShowInfo] = useState(false);
   return (
-      <div className='question'>
-        <p>
-          <h4 className='header'>
-            {title}
-            <button className='btn' onClick={() => {
-              setIsShowInfo(!isShowInfo)
-            }}>
-              { isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus/>}
-            </button>
-          </h4>
-        </p>
-        <p>{isShowInfo ? info : ""}</p>
-      </div>
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => {
+            setIsShowInfo(!isShowInfo)
+        }}>
+          { isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      <p>
+          
+      </p>
+      <p>{isShowInfo ? info : ""}</p>
+    </article>  
   )
 };
 
