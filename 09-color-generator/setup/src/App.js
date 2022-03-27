12c36a3761a75400ddb86efbfe7889b7504031db
Yayawak <elio.interesante@gmail.com>
2022-03-27 19:20:14 +0700

    Finised color generator, try to add feature like adding frequency button to change amount of colors.

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,60 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
-
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor ] = useState('');
+  const [isError, setIsError] = useState(false);
+  // const [freq, setFreq] = useState(20);
+  const [list, setList] = useState(new Values('#f1ff25').all(5));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(5);
+      // console.log(colors)
+      setList(colors)
+    } 
+    catch(error) {
+      setIsError(true);
+      // alert(error);
+      console.error(error)
+    }
+  }
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={color} 
+            onChange={(e) => setColor(e.target.value) } 
+            placeholder="#f15025"
+            className={`${isError ? 'error' : null}`} />
+          <button className='btn' type='submit'>submit</button>
+        </form>
+        {/* <form>
+          <input type='number' value={freq} 
+            onChange={(e) => {
+              parseInt(setFreq(e.target.value))
+              setList(new Values(color).all(freq))
+            }} 
+            placeholder={freq}
+            className='' />
+        </form> */}
+      </section>
+
+      <section className='colors'>
+      {list.map((_color,ind) => {
+          // console.log(_color)
+
+          return(
+            <SingleColor key={ind} {..._color} index={ind} hexColor={_color.hex} />
+          )
+        })}
+      </section>
+    </>
+    
+  )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index, hexColor,  }) => {
+  const [isAlert,setIsAlert] = useState(false);
+  const bcg = rgb.join(',');
+  // const hex = rgbToHex(...rgb); // this alrady has prefix #, hexColor has no # as prefix.
+  const hexValue = `#${hexColor}`
+  // console.log(hexValue)
+  // console.log(bcg)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsAlert(false)
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [isAlert])
+  const divinderIndex = (20);
+  // console.log(divinderIndex)
+  return (
+    <article className={`color ${index > divinderIndex  && 'color-light'}`} 
+      style={{backgroundColor:`rgb(${bcg})`}}
+      onClick={() => {
+        setIsAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}>
+      <p className='percent-value'>
+        {weight}%
+      </p>
+      <p className='color-value'>{hexValue}</p>
+      {/* if alert is true, then do make <p>-tag */}
+      {isAlert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
