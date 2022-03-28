f2c77f2fb78697f8e6381264e36a681a727e9e93
Yayawak <elio.interesante@gmail.com>
2022-03-28 17:38:02 +0700

    complete Sidebar & Modal project with useContext hook.

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,40 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+// wrap whole App into AppProvider
+const AppProvider = ({children}) => {
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false)
+    const [ isModalOpen, setIsModalOpen ] = useState(false)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+    return <AppContext.Provider value={
+        // sent object with no defalut value
+        {
+        isModalOpen, isSidebarOpen, 
+        openModal, openSidebar,
+        closeModal, closeSidebar
+
+        }
+    }>
+        {children}
+    </AppContext.Provider>
+}
+
+
+// custom hook, reusable function like useFetch(url)-hook.
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export {AppContext, AppProvider};
