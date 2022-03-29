0bc7f58c20c08558d41c4a6609537f448ceb47e6
Yayawak <elio.interesante@gmail.com>
2022-03-30 00:20:40 +0700

    fixed increase and success in making decreade amount button.

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -18,12 +18,20 @@ const AppProvider = ({ children }) => {
   const remove = (id) => {
     dispatch({type:'REMOVE', payload:id})
   }
+  const increase = id => {
+    dispatch({type:'INCREASE', payload:id})
+  }
+  const decrease = id => {
+    dispatch({type:'DECREASE', payload:id})
+  }
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
-        remove
+        remove,
+        increase,
+        decrease
       }}
     >
       {children}
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -12,6 +12,28 @@ const reducer = (state,action) => {
             ),
         }
     }
+    
+    if (action.type === 'INCREASE'){
+        let tempCart = state.cart.map(cartItem => {
+            if (cartItem.id === action.payload){
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem
+        })
+        return { ...state, cart: tempCart}
+    }
+    if (action.type === 'DECREASE'){
+        let tempCart = state.cart
+            .map(cartItem => {
+                if (cartItem.id === action.payload){
+                    return {...cartItem, amount: cartItem.amount - 1 }
+                }
+                return cartItem
+        })
+        .filter(cartItem => cartItem.amount !==0 )
+        
+        return { ...state, cart: tempCart}
+    }
     return state;
 }
 
