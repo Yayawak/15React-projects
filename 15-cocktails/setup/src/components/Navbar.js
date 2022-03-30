cad4901ce434f4fdb8be1b366e4f552329b6589b
Yayawak <elio.interesante@gmail.com>
2022-03-30 15:34:10 +0700

    set up basic Ui and fixed Routes Not found in importation.

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -4,9 +4,26 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' 
+            className='logo'/>
+        </Link>
+      </div>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/' >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/about'>
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
